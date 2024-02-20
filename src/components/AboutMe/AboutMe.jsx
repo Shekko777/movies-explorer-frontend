b6d584404efde2a851538c445af62adea668938b
Ivan Pershin <shekko-777@yandex.ru>
2024-02-20 12:48:08 +0300

    fix: Доработал доработку

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -14,7 +14,7 @@ const AboutMe = () => {
               и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
             <a className="about-me__github-link" href="https://github.com/Shekko777" target="_blank" rel="noopener noreferrer">Github</a>
           </div>
-          <img className="about-me__avatar" src={avatarJpg} alt="Моя фотография" />
+          <img className="about-me__avatar" src={avatarJpg} alt="Фотография моего профиля на которой изображен Я" />
         </div>
       </div>
 
